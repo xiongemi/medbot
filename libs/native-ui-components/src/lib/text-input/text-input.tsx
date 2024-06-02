e0006eaa6fcd8adc55766334ea9b4a729438e5a2
Venkatesh Morla <morlavenkatesh19@gmail.com>
2024-06-02 17:21:33 +0530

    storybook added

---
+++ libs/native-ui-components/src/lib/text-input/text-input.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+/* eslint-disable-next-line */
+export interface TextInputProps {}
+
+export function TextInput(props: TextInputProps) {
+  return (
+    <View>
+      <Text>Welcome to text-input!</Text>
+    </View>
+  );
+}
+
+export default TextInput;
